9a1b00cd1b3cf8e75bbc67101f3687bef203cc32
Pawel <pawflor@gmail.com>
2025-05-11 22:23:03 +0200

    plots_init

--- experiments/utils.py
+++ experiments/utils.py
@@ -128,8 +128,9 @@ class BinaryDatasetManager:
         test_to_valid_ratio: float = 0.5,
         random_state: int = RANDOM_SEED,
         oversampler: Any | None = None,
-        preprocessing_pipeline_creator: Callable[[np.ndarray], ColumnTransformer]
-        | None = None,
+        preprocessing_pipeline_creator: (
+            Callable[[np.ndarray], ColumnTransformer] | None
+        ) = None,
     ) -> Iterator[
         Tuple[
             str,
@@ -505,9 +506,7 @@ class ParamRunner(BaseEstimator):
         if option == "bagging":
             n_bagging_estimators = params.pop("n_bagging_estimators")
             model = bagging_classifier_class(
-                base_estimator=base_estimator_class(
-                    **params, random_state=random_state
-                ),
+                estimator=base_estimator_class(**params, random_state=random_state),
                 n_estimators=n_bagging_estimators,
                 random_state=random_state,
             )
@@ -522,7 +521,9 @@ class ParamRunner(BaseEstimator):
                 if name == "roc_auc":
                     if hasattr(model, "predict_proba"):
                         y_proba = model.predict_proba(X)[:, 1]
-                        mode_scores[name] = scorer._score_func(y, y_proba)  # pylint: disable=protected-access
+                        mode_scores[name] = scorer._score_func(
+                            y, y_proba
+                        )  # pylint: disable=protected-access
                     else:
                         mode_scores[name] = None
                 else:
@@ -575,6 +576,47 @@ class ParamRunner(BaseEstimator):
             ):
                 results.append(result)
         return results
+
+
+def read_results(run_id: int = 0, results_dir: str = RESULTS_DIR) -> pd.DataFrame:
+    """
+    Read the results from the specified run directory and return a DataFrame.
+
+    Args:
+        run_id (int, optional): The ID of the run to read results from.
+        results_dir (str, optional): The directory where the results are stored.
+    Returns:
+        pd.DataFrame: A DataFrame containing the results of the specified run.
+    """
+    run_dir = os.path.join(results_dir, f"run_{run_id}")
+
+    results_df = None
+    for file in os.listdir(run_dir):
+        if file.endswith(".pkl"):
+            (dataset_name, smote_name, model_name, option, take) = os.path.basename(
+                os.path.splitext(file)[0]
+            ).split("__")
+            cur_df = pd.read_json(
+                os.path.join(run_dir, file), orient="records", lines=True
+            )
+            cur_df["dataset_name"] = dataset_name
+            cur_df["smote_name"] = smote_name
+            cur_df["model_name"] = model_name
+            cur_df["option"] = option
+            cur_df["take"] = take
+            cur_df["run_id"] = run_id
+
+            for stage in ["train", "test"]:
+                for key in ["accuracy", "f1", "precision", "recall", "roc_auc"]:
+                    cur_df[f"{stage}_{key}"] = cur_df[stage].map(lambda x: x[key])
+            cur_df = cur_df.drop(columns=["train", "test"])
+
+            if results_df is None:
+                results_df = cur_df
+            else:
+                results_df = pd.concat([results_df, cur_df], ignore_index=True)
+
+    return results_df
 
 
 def create_directories():
